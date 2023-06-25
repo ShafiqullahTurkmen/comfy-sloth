638bee71e21cf80d919d01b8c363280721ea7efc
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-06-25 08:54:04 +0300

    react router setup added

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./HomePage";
+import Products from "./ProductsPage";
+import SingleProduct from "./SingleProductPage";
+import About from "./AboutPage";
+import Cart from "./CartPage";
+import Error from "./ErrorPage";
+import Checkout from "./CheckoutPage";
+import Private from "./PrivateRoute";
+
+export { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private };
