54420badadf468142d4ac44877d0d8332457f598
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-05 21:23:44 +0300

    Home Page - Services completed

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,25 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return <Wrapper>
+    <div className='section-center'>
+      <article className='header'>
+        <h3>custom furniture <br /> built only for you</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis voluptatibus molestias iure eligendi beatae. Tempora eaque tempore inventore necessitatibus iste!</p>
+      </article>
+      <div className='services-center'>
+        {
+          services.map(({id, icon, title, text}) => (
+            <article key={id} className='service'>
+              <span className='icon'>{icon}</span>
+              <h4>{title}</h4>
+              <p>{text}</p>
+            </article>
+          ))
+        }
+      </div>
+    </div>
+  </Wrapper>
 }
 
 const Wrapper = styled.section`
