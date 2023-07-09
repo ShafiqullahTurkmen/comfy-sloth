b7c78eb9a13ef718bbaac4d264eb0f9526a69385
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-09 09:41:08 +0300

    single product base return added

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -36,11 +36,51 @@ const SingleProductPage = () => {
     }
   }, [error])
 
-
   if (loading) return <Loading />
   if (error) return <Error />
+  const {
+    name,
+    price,
+    description,
+    stock,
+    reviews,
+    id: sku,
+    company,
+    images
+  } = product;
 
-  return <h4>single product page</h4>;
+  return <Wrapper>
+    <PageHero title={name} product={product}/>
+    <div className="section section-center page">
+      <Link to="/products" className="btn">
+        back to products
+      </Link>
+      <div className="product-center">
+        <ProductImages/>
+        <section className="content">
+          <h2>{name}</h2>
+          <Stars />
+          <h5 className="price">{formatPrice(price)}</h5>
+          <p className="desc">{description}</p>
+          <p className="info">
+            <span>Available : </span>
+            { stock > 0 ? "In stock" : "out of stock"}
+          </p>
+          <p className="info">
+            <span>SKU : </span>
+            { sku}
+          </p>
+          <p className="info">
+            <span>Brand : </span>
+            {company}
+          </p>
+          <hr />
+          {stock > 0 && <AddToCart/>}
+        </section>
+      </div>
+    </div>
+
+  </Wrapper>;
 };
 
 const Wrapper = styled.main`
