447eb946588e7f8d1bed53396755346e8d1912c3
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-17 21:22:09 +0300

    single product add to cart - amount buttons added

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -9,7 +9,22 @@ const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(Array.isArray(colors) && colors[0]);
+  const [amount, setAmount] = useState(1);
 
+  const increase = () => {
+    setAmount(oldAmount => {
+      let tempAmount = oldAmount + 1
+      if (tempAmount > stock) tempAmount = stock;
+      return tempAmount;
+    })
+   }
+  const decrease = () => {
+    setAmount(oldAmount => {
+      let tempAmount = oldAmount - 1
+      if (tempAmount < 1) tempAmount = 1;
+      return tempAmount;
+    })
+   }
 
   return <Wrapper>
     <div className="colors">
@@ -23,14 +38,21 @@ const AddToCart = ({ product }) => {
               style={{ background: color }}
               onClick={() => setMainColor(color)}
             >
-              { mainColor === color ? <FaCheck/> : null}
+              {mainColor === color ? <FaCheck /> : null}
             </button>
           ))
         }
       </div>
     </div>
     <div className="btn-container">
-
+      <AmountButtons
+        amount={amount}
+        increase={increase}
+        decrease={decrease}
+      />
+      <Link to="/cart" className="btn">
+        add to cart
+      </Link>
     </div>
   </Wrapper>
 }
