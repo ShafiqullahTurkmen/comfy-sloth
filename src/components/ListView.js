513a29457b2bdc5d3ec2e71776bb2ca1f844e459
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-17 22:26:10 +0300

    products page list view added

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -2,8 +2,22 @@ import React from 'react'
 import styled from 'styled-components'
 import { formatPrice } from '../utils/helpers'
 import { Link } from 'react-router-dom'
-const ListView = () => {
-  return <h4>list view</h4>
+const ListView = ({ products }) => {
+  return <Wrapper>{
+    products.map(({ id, image, name, price, description }) => (
+      <article key={id}>
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <h5 className="price">{formatPrice(price)}</h5>
+          <p>{description.substring(0, 150)}...</p>
+          <Link to={`/products/${id}`} className="btn">
+            Details
+          </Link>
+        </div>
+      </article>
+    ))
+  }</Wrapper>
 }
 
 const Wrapper = styled.section`
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,15 @@ import GridView from './GridView'
 import ListView from './ListView'
 
 const ProductList = () => {
-  const { filtered_products: products } = useFilterContext();
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  if (products.length < 1) return (
+    <h5 style={{ textTransform: "none" }}>Sorry, no products matched you search...</h5>
+  )
+
+  if (!grid_view) return <ListView products={products} />
+
+
   return <GridView products={products}>product list</GridView>
 }
 
