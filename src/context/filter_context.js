1888227a00bb5512840a81cf2bf3bc9fb680f0f5
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-18 07:07:19 +0300

    sort component view buttons added

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,7 +15,7 @@ import { useProductsContext } from "./products_context";
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: true
+  grid_view: true,
 };
 
 const FilterContext = React.createContext();
@@ -26,10 +26,19 @@ export const FilterProvider = ({ children }) => {
 
   useEffect(() => {
     dispatch({ type: LOAD_PRODUCTS, payload: products });
-  }, [products])
+  }, [products]);
+
+  const setGridView = () => {
+    dispatch({ type: SET_GRIDVIEW });
+  };
+
+  
+  const setListView = () => {
+    dispatch({ type: SET_LISTVIEW });
+  };
 
   return (
-    <FilterContext.Provider value={{...state}}>
+    <FilterContext.Provider value={{ ...state, setGridView, setListView }}>
       {children}
     </FilterContext.Provider>
   );
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -17,6 +17,15 @@ const filter_reducer = (state, action) => {
       filtered_products: [...action.payload],
     };
   }
+
+  if (action.type === SET_GRIDVIEW) {
+    return { ...state, grid_view: true };
+  }
+
+  if (action.type === SET_LISTVIEW) {
+    return { ...state, grid_view: false };
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
