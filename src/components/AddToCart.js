ba40f803e81ede87f8b4c1f28b5205baac678b8f
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-20 20:54:13 +0300

    add to cart setup added

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext();
   const { id, stock, colors } = product;
 
   const [mainColor, setMainColor] = useState(Array.isArray(colors) && colors[0]);
@@ -17,14 +18,14 @@ const AddToCart = ({ product }) => {
       if (tempAmount > stock) tempAmount = stock;
       return tempAmount;
     })
-   }
+  }
   const decrease = () => {
     setAmount(oldAmount => {
       let tempAmount = oldAmount - 1
       if (tempAmount < 1) tempAmount = 1;
       return tempAmount;
     })
-   }
+  }
 
   return <Wrapper>
     <div className="colors">
@@ -50,7 +51,7 @@ const AddToCart = ({ product }) => {
         increase={increase}
         decrease={decrease}
       />
-      <Link to="/cart" className="btn">
+      <Link to="/cart" className="btn" onClick={() => addToCart(id, mainColor, amount, product)}>
         add to cart
       </Link>
     </div>
