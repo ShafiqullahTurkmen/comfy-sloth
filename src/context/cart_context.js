d3f1db6490254aeb6dea7e781c7e078696ad424f
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-21 18:18:39 +0300

    localstorage setup added

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -8,8 +8,13 @@ import {
   COUNT_CART_TOTALS,
 } from "../actions";
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  return (cart && JSON.parse(cart)) || [];
+};
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -27,6 +32,10 @@ export const CartProvider = ({ children }) => {
   const removeItem = (id) => {};
   const toggleAmount = (id, value) => {};
   const clearCart = () => {};
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider
