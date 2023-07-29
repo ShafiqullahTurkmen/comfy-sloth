02b5747acd5afc1dbcb38f9a79e5232e5723b584
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-29 09:25:53 +0300

    Auth0 and user context setup added

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -65,7 +65,9 @@ export const ProductsProvider = ({ children }) => {
   }, []);
 
   return (
-    <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}>
+    <ProductsContext.Provider
+      value={{ ...state, openSidebar, closeSidebar, fetchSingleProduct }}
+    >
       {children}
     </ProductsContext.Provider>
   );
--- src/index.js
+++ src/index.js
@@ -11,12 +11,29 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+console.log({
+  domain: process.env.REACT_APP_DOMAIN,
+  client: process.env.REACT_APP_CLIENT_ID,
+  secret: process.env.REACT_APP_CLIENT_SECRET,
+});
+
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+
+  <Auth0Provider
+    domain="dev-6sg1tpa4.us.auth0.com"
+    clientId="u3EmFCkMjEbxuZ0veqdMGuldyugW15R2"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
 );
