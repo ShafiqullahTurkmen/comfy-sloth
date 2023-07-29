819288a668ffcef93ab81d74eeb095200d53ed28
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-29 10:08:48 +0300

    logout button added

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -9,6 +9,8 @@ import { useUserContext } from '../context/user_context'
 const CartButtons = () => {
   const { isSidebarOpen, openSidebar, closeSidebar } = useProductsContext();
   const { total_items } = useCartContext();
+  const { loginWithRedirect, logout, myUser } = useUserContext();
+
   return (
     <Wrapper className='cart-btn-wrapper'>
       <Link onClick={closeSidebar} to='/cart' className='cart-btn'>
@@ -18,8 +20,11 @@ const CartButtons = () => {
           <span className='cart-value'>{total_items}</span>
         </span>
       </Link>
-      <button type='button' className='auth-btn'>
-        Login <FaUserPlus/>
+      <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+        Login <FaUserPlus />
+      </button>
+      <button type='button' className='auth-btn' onClick={() => logout({ returnTo: window.location.origin })}>
+        Logout <FaUserMinus />
       </button>
     </Wrapper>
   )
--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,8 +3,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
-  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } =
-    useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, user, isLoading } = useAuth0();
 
   const [myUser, setMyUser] = useState(null);
 
