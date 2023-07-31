37bca561d520f074c1fbece5d5f2be01faad0fdc
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2023-07-31 22:02:26 +0300

    stripe checkout complete

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -63,11 +63,48 @@ const CheckoutForm = () => {
     // eslint-disable-next-line
   }, []);
 
-  const handleChange = async (event) => {};
-  const handleSubmit = async (ev) => {};
+  const handleChange = async (event) => {
+    setDisabled(event.empty)
+    setError(event.error ? event.error.message : "")
+  };
+  const handleSubmit = async (ev) => {
+    ev.preventDefault()
+    setProcessing(true)
+    const payload = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement)
+      }
+    })
+    if (payload.error) {
+      setError(`Payment failed ${payload.error.message}`)
+      setProcessing(false)
+    } else {
+      setError(null)
+      setProcessing(false)
+      setSucceeded(true)
+      setTimeout(() => {
+        clearCart();
+        history.push("/")
+      }, 10000);
+    }
+  };
 
   return (
     <div>
+      {
+        succeeded ?
+          <article>
+            <h4>Thank you</h4>
+            <h4>Your payment was successfull!</h4>
+            <h4>Redirection to home page shortly</h4>
+          </article>
+          :
+          <article>
+            <h4>Hello, {myUser && myUser.name}</h4>
+            <p>Your total is {formatPrice(shipping_fee + total_amount)}</p>
+            <p>Test Card Number: 4242 4242 4242 4242</p>
+          </article>
+      }
       <form id="payment-form" onSubmit={handleSubmit}>
         <CardElement
           id="card-element"
